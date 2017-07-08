74c93e126bb158ccc21155fcaa061dd69622c9fd
Maegan Womble <maeganwomble@gmail.com>
2017-07-08 16:45:44 -0500

    refactored and ready for ajax test

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 $(document).ready(function () {
-
+    
     $("button").click(function () {
-        if ($("option:selected").val() == 'Marzett') {
-            $.getJSON('data.json', function (dataObj) {
-                var outputKey = "";
-                var resultOne = dataObj.result.records[0];
+        $.ajax({url: "http://localhost:9000/data-services/v3/commingling/ok?startDate=2017-06-13&endDate=2017-07-06&shapeFormat=geojson&docTypeVersion=v2&page=1&token=7781e0c3-2d45-480f-b80f-f55480233a96", success: function(dataObj){
+            var outputKey = "";
+            
+            //Well Constructor Function
+            var Well = function (index) {
+                this.result = dataObj.result.records[index];
+                
+                //Update Function
+                this.update = function () {
+                    for (key in this.result) {
+                        formattedKey = key.toUpperCase(key).replace(/_/g, " ");
 
-                for (key in resultOne) {
-                    formattedKey = key.toUpperCase(key).replace(/_/g, " ");
-
-                    outputKey += '<li><span class="key">' + formattedKey + '</span> : <span class="value">' + resultOne[key] + '</span></li>';
+                        outputKey += '<li><span class="key">' + formattedKey + '</span> : <span class="value">' + this.result[key] + '</span></li>';
+                    }
+                    $(".records-container").html(outputKey.replace(/;/g, " "));
                 }
+            }
+            
+            //Construct Wells
+            var marzett = new Well(0);
+            var johnsonState = new Well(1);
+            
+            //Logic
+            if ($("option:selected").val() == 'Marzett') {
+                marzett.update();
+            } else if ($("option:selected").val() == 'Johnson State') {
+                johnsonState.update();
+            } else {
+                $(".records-container").html(outputKey);
+            };
 
-                $(".records-container").html(outputKey.replace(/;/g, " "));
-            });
-        } else if ($("option:selected").val() == 'Johnson State') {
-            $.getJSON('data.json', function (dataObj) {
-                var outputKey = "";
-                var resultTwo = dataObj.result.records[1];
-
-                for (key in resultTwo) {
-                    formattedKey = key.toUpperCase(key).replace(/_/g, " ");
-
-                    outputKey += '<li><span class="key">' + formattedKey + '</span> : <span class="value">' + resultTwo[key] + '</span></li>';
-                }
-
-                $(".records-container").html(outputKey.replace(/;/g, " "));
-            });
-        } else {
-            outputKey = "";
-            $(".records-container").html(outputKey);
-        };
+        }});
     });
 });
